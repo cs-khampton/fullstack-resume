5ae3ed46d1bcaaccbf9d4bae2816bc8ace32988c
Kai Hampton <kaili.hampton@colostate.edu>
2026-09-10 14:46:19 -0600

    update json and css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,14 @@ function App() {
   return (
     <>
       <h3>Hello & Welcome!</h3>
-      <p>My name is Kai; I was raised in Central Wyoming after being adopted from China. Shortly after finishing high school in 2015 I moved to Fort Collins, Colorado where I currently reside.
+      <p className="body">
+        My name is Kai - I grew up in Lander, Wyoming where my curiousity about software and technology began.
+        My father was a web designer who gave me the opportunity to start learning HTML/CSS around the age of 8.
+        Growing up, a family friend worked in the National Outdoor Leadership School (NOLS) IT department and allowed me to tinker around with electronics, taking apart various types of computers while my mother was in meetings.
+
+        In 2017, I moved to Fort Collins, Colorado where I continued my academic journey in 2019 at Front Range Community College. After earning my Associate's Degree in Computer Programming,
+        I transferred to Colorado State University to further my technical skills and to simply keep learning.
+
         In 2023 I completed my Associate's Degree in Computer Programming at Front Range Community College and transferred to Colorado State University where I earned my Bachelor's Degree in Computer Science during the summer of 2026.
         This site is a showcase of the skills I've built over the course of my academic career along with those I am actively exploring and practicing.
         <br></br><br></br>To learn more about me, click the links in the navigation bar above!</p>
