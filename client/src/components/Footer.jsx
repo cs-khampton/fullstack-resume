20e07d937e6a161ac477a966c34765f59f5d433d
Kai Hampton <kaili.hampton@colostate.edu>
2026-09-25 10:31:50 -0600

    add reminder comment

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,12 +1,12 @@
 import '../styles/footer.css';
 
 function Footer() {
-    // XXX: Do I move this into the contact page?
     return (
         <footer>
             <p>Kaili Hampton</p>
-            <a target="_blank" href="https://www.linkedin.com/in/kai-hampton726">LinkedIn</a>
-            <a target="_blank" href="https://github.com/cs-khampton/">GitHub</a>
+            {/* XXX: Looks empty without links... */}
+            {/* <a target="_blank" href="https://www.linkedin.com/in/kai-hampton726">LinkedIn</a>
+            <a target="_blank" href="https://github.com/cs-khampton/">GitHub</a> */}
         </footer>
     )
 }
