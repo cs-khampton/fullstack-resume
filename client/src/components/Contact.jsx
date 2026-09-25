6e31f35948af45b99f171ef9f85311547494ded2
Kai Hampton <kaili.hampton@colostate.edu>
2026-09-25 10:29:33 -0600

    move footer to contact page

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -5,6 +5,8 @@ function Contact() {
             <div className='body'>
                 <p id='phone'>Call Me: <Link to="tel:+13074383537">(307) 438-3537</Link></p>
                 <p id='email'>Email Me: <Link to="mailto:kailixian97@gmail.com">kailixian97@gmail.com</Link></p>
+                <p id='linkedin'>Visit my LinkedIn Profile: <a target="_blank" href="https://www.linkedin.com/in/kai-hampton726">LinkedIn</a></p>
+                <p id='github'>Visit my GitHub Profile: <a target="_blank" href="https://github.com/cs-khampton/">GitHub</a></p>
             </div>
         </>
     )
